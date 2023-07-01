50508ef03e407bbe24544219130ae013fb9323a1
Tilrk Torogeldiev <tilek2402@gmail.com>
2023-07-02 01:46:17 +0600

    sorry braza

---
+++ medium/top_k_frequent_elements/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func topKFrequent(nums []int, k int) []int {
+	numOccurences := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		numOccurences[nums[i]]++
+	}
+	sortedOccurences := []int{}
+	for _, occurences := range numOccurences {
+		sortedOccurences = append(sortedOccurences, occurences)
+	}
+	changed := false
+	sort.Slice(sortedOccurences, func(i, j int) bool {
+		if sortedOccurences[i] > sortedOccurences[j] || sortedOccurences[i] < sortedOccurences[j] {
+			changed = true
+		}
+		return sortedOccurences[i] > sortedOccurences[j]
+	})
+	if !changed && len(sortedOccurences) != 1 && len(sortedOccurences) != k {
+		return []int{0}
+	}
+	res := []int{}
+one:
+	for i := 0; i < len(sortedOccurences); i++ {
+		for number, occurences := range numOccurences {
+			if sortedOccurences[i] == occurences {
+				res = append(res, number)
+				delete(numOccurences, number)
+				if len(res) == k {
+					break one
+				}
+			}
+		}
+	}
+	return res
+}
+
+func main() {
+	fmt.Println("Salam Aleikum")
+	fmt.Println(topKFrequent([]int{1}, 2))
+}
