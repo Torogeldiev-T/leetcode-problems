d409b3627c6100e5688cecdd244f6ab8bdd98f8a
Tilek Torogeldiev <tilek2402@gmail.com>
2025-10-26 19:51:18 +0600

    binary search

---
+++ easy/binary_search/main.ts
@@ -0,0 +1,21 @@
+function search(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        console.log('current mid', mid, 'left', left, 'right', right);
+
+        if (nums[mid] > target) {
+            right = mid - 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+};
+
+console.log(search([5], 5));
+
