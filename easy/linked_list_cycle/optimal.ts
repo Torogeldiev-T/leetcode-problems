2755b1194bf6c4780ff6d0200425a88ad24c23a7
Tilek Torogeldiev <tilek2402@gmail.com>
2026-02-21 18:24:23 +0600

    Update configs

--- easy/linked_list_cycle/optimal.ts
+++ easy/linked_list_cycle/optimal.ts
@@ -1,14 +1,11 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
 
 export function hasCycle(head: ListNode | null): boolean {
     let slow: ListNode = head;
